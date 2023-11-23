5d95e843307350e56ec485a44c5961a7f3924121
AzizEmir <korkmazazizemir@gmail.com>
2023-11-23 22:46:06 +0300

    1$

---
+++ README.md
@@ -0,0 +1,29 @@
+# Script Purpose
+
+This Bash script is designed to remove specific software packages from a Debian-based Linux system. It targets a predefined list of packages associated with GNOME games and utilities. Additionally, it attempts to clean up residual dependencies after the removal process.
+
+# Genel Amaç
+
+Bu Bash betiği, Debian tabanlı bir Linux sisteminden belirli yazılım paketlerini kaldırmak için tasarlanmıştır. Önceden belirlenmiş bir GNOME oyunları ve yardımcı programları listesini hedef alır. Ayrıca, kaldırma işleminden sonra kalan bağımlılıkları temizlemeye çalışır.
+
+
+Silnen Paketler / Deleted packages
+1. gnome-2048
+2. aisleriot
+3. gnome-nibbles
+4. five-or-more
+5. four-in-a-row
+6. hitori
+7. lightsoff
+8. gnome-klotski
+9. gnome-mahjongg
+10. gnome-mines
+11. quadrapassel
+12. iagno
+13. gnome-robots
+14. gnome-chess
+15. gnome-sudoku
+16. swell-foop
+17. tali
+18. gnome-taquin
+19. gnome-tetravex
---
+++ debian12debloat.sh
@@ -0,0 +1,82 @@
+#!/usr/bin/env bash
+
+set -e # Exit the script on error
+
+# Determine user's locale
+if [[ $LANG == *"tr_TR"* ]]; then
+    LOCALE="TR"
+else
+    LOCALE="EN"
+fi
+
+# Function to print messages based on the locale
+print_message() {
+    if [ "$LOCALE" == "TR" ]; then
+        echo "$1"
+    else
+        echo "$2"
+    fi
+}
+
+if [[ $(id -u) -ne 0 ]]; then
+    print_message "Bu betiği yönetici olarak çalıştırmalısınız. Lütfen yönetici olarak çalıştırın." \
+        "You must run this script as an administrator. Please run it as an administrator."
+    exit 1
+else
+    # List of Programs to be Removed
+    COLOR_ERROR='\033[0;31m'
+    packages=(
+        gnome-2048
+        aisleriot
+        gnome-nibbles
+        five-or-more
+        four-in-a-row
+        hitori
+        lightsoff
+        gnome-klotski
+        gnome-mahjongg
+        gnome-mines
+        quadrapassel
+        iagno
+        gnome-robots
+        gnome-chess
+        gnome-sudoku
+        swell-foop
+        tali
+        gnome-taquin
+        gnome-tetravex
+    )
+    counter=1
+
+    for package in "${packages[@]}"; do
+        print_message "$counter - $package paketi kontrol ediliyor..." \
+            "$counter - Checking package $package..."
+        if dpkg -s "$package" &>/dev/null; then
+            print_message "$package paketi bulundu. Kaldırılıyor..." \
+                "$package package found. Removing..."
+            if apt purge -y "$package" >/dev/null 2>&1; then
+                print_message "$package paketi kaldırıldı." \
+                    "$package package removed."
+                echo ""
+            else
+                error_code=$?
+                print_message "${COLOR_ERROR}Hata: $package paketi kaldırılırken bir sorun oluştu. Hata Kodu: $error_code" \
+                    "${COLOR_ERROR}Error: There was a problem removing the $package package. Error Code: $error_code"
+                exit 1
+            fi
+
+        else
+            print_message "$package paketi bulunamadı." \
+                "$package package not found."
+            echo ""
+        fi
+        ((counter++))
+    done
+
+    print_message "Kalıntı Bağımlılıklar Siliniyor" \
+        "Removing Residual Dependencies"
+    if apt autoremove >/dev/null 2>&1; then
+        print_message "Kalıntılar silindi" \
+            "Residual dependencies removed"
+    fi
+fi
