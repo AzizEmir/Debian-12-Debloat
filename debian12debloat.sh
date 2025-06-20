67ff196dc181c5ce5f0980683523f25c87f19bc5
hugorsz-dev <120595249+hugorsz-dev@users.noreply.github.com>
2025-06-20 21:08:09 +0200

    Update debian12debloat.sh
        The following improvements are to be made:
        
        -       Scripting is to be improved.
        
        -       The English language is to be standardised.
        
        -       All GNOME packages are to be incorporated.
        
        -       These are to be separated into groups to favour a selective and user-selective 'debloat' according to user preference.

--- debian12debloat.sh
+++ debian12debloat.sh
@@ -2,30 +2,36 @@
 
 set -e # Exit the script on error
 
-# Determine user's locale
-if [[ $LANG == *"tr_TR"* ]]; then
-    LOCALE="TR"
-else
-    LOCALE="EN"
-fi
-
-# Function to print messages based on the locale
-print_message() {
-    if [ "$LOCALE" == "TR" ]; then
-        echo "$1"
-    else
-        echo "$2"
-    fi
+remove_packages() {
+   local packages=("$@")
+   
+   for package in "${packages[@]}"; do
+       echo "Checking package $package..."
+       if dpkg -s "$package" &>/dev/null; then
+           echo "$package package found. Removing..."
+           if apt purge -y "$package" >/dev/null 2>&1; then
+               echo "$package package removed."
+               echo ""
+           else
+               error_code=$?
+               echo "${COLOR_ERROR}Error: There was a problem removing the $package package. Error Code: $error_code"
+               exit 1
+           fi
+       else
+           echo "$package package not found."
+           echo ""
+       fi
+   done
 }
 
 if [[ $(id -u) -ne 0 ]]; then
-    print_message "Bu betiği yönetici olarak çalıştırmalısınız. Lütfen yönetici olarak çalıştırın." \
-        "You must run this script as an administrator. Please run it as an administrator."
+    echo "You must run this script as an administrator. Please run it as an administrator."
     exit 1
 else
     # List of Programs to be Removed
     COLOR_ERROR='\033[0;31m'
-    packages=(
+
+    packages_games=(
         gnome-2048
         aisleriot
         gnome-nibbles
@@ -46,37 +52,107 @@ else
         gnome-taquin
         gnome-tetravex
     )
-    counter=1
 
-    for package in "${packages[@]}"; do
-        print_message "$counter - $package paketi kontrol ediliyor..." \
-            "$counter - Checking package $package..."
-        if dpkg -s "$package" &>/dev/null; then
-            print_message "$package paketi bulundu. Kaldırılıyor..." \
-                "$package package found. Removing..."
-            if apt purge -y "$package" >/dev/null 2>&1; then
-                print_message "$package paketi kaldırıldı." \
-                    "$package package removed."
-                echo ""
-            else
-                error_code=$?
-                print_message "${COLOR_ERROR}Hata: $package paketi kaldırılırken bir sorun oluştu. Hata Kodu: $error_code" \
-                    "${COLOR_ERROR}Error: There was a problem removing the $package package. Error Code: $error_code"
-                exit 1
-            fi
+    packages_info=(
+        gnome-weather
+        yelp
+        gnome-maps
+        gnome-characters
+        gnome-font-viewer
+    )
 
-        else
-            print_message "$package paketi bulunamadı." \
-                "$package package not found."
-            echo ""
-        fi
-        ((counter++))
-    done
+    packages_productivity=(
+        evolution
+        gnome-clocks
+        gnome-calendar
+        gnome-calculator
+        gnome-contacts
+    )
 
-    print_message "Kalıntı Bağımlılıklar Siliniyor" \
-        "Removing Residual Dependencies"
+    packages_bloat_multimedia=(
+        gnome-music
+        rhythmbox
+        shotwell
+        simple-scan
+        gnome-sound-recorder
+    )
+
+    packages_bloat_system=(
+        seahorse
+        gnome-logs
+        gnome-tweaks
+        file-roller
+    )
+
+    packages_multimedia=(
+        totem
+        eog
+    )
+
+    packages_system=(
+        network-manager-gnome
+        gnome-system-monitor
+        baobab
+        gnome-disk-utility
+        nautilus
+    )
+
+    packages_system=(
+        network-manager-gnome
+        gnome-system-monitor
+        baobab
+        gnome-disk-utility
+        nautilus
+    )
+
+    package_libreoffice=(
+        libreoffice-*
+    )
+
+    read -p "Delete all gnome games? (Recommended) [Y/n] " response
+    if [[ "$response" =~ ^[Yy]([Ee][Ss])?$ ]] || [[ -z "$response" ]]; then
+        remove_packages "${packages_games[@]}"
+    fi
+    
+    read -p "Delete all gnome info packages? (Recommended) [Y/n] " response
+    if [[ "$response" =~ ^[Yy]([Ee][Ss])?$ ]] || [[ -z "$response" ]]; then
+        remove_packages "${packages_info[@]}"
+    fi
+    
+    read -p "Delete all gnome productivity packages? (Recommended) [Y/n] " response
+    if [[ "$response" =~ ^[Yy]([Ee][Ss])?$ ]] || [[ -z "$response" ]]; then
+        remove_packages "${packages_productivity[@]}"
+    fi
+    
+    read -p "Delete all gnome bloat multimedia packages? (Recommended) [Y/n] " response
+    if [[ "$response" =~ ^[Yy]([Ee][Ss])?$ ]] || [[ -z "$response" ]]; then
+        remove_packages "${packages_bloat_multimedia[@]}"
+    fi
+    
+    read -p "Delete all gnome bloat system packages? (Recommended) [Y/n] " response
+    if [[ "$response" =~ ^[Yy]([Ee][Ss])?$ ]] || [[ -z "$response" ]]; then
+        remove_packages "${packages_bloat_system[@]}"
+    fi
+
+    read -p "Delete all libreoffice packages? [Y/n] " response
+    if [[ "$response" =~ ^[Yy]([Ee][Ss])?$ ]]; then
+        remove_packages "${packages_libreoffice[@]}"
+    fi
+
+    read -p "Delete all gnome multimedia packages? (NOT Recommended) [Y/n] " response
+    if [[ "$response" =~ ^[Yy]([Ee][Ss])?$ ]]; then
+        remove_packages "${packages_multimedia[@]}"
+    fi
+    
+    read -p "Delete all gnome system packages? (NOT Recommended) [Y/n] " response
+    if [[ "$response" =~ ^[Yy]([Ee][Ss])?$ ]]; then
+        remove_packages "${packages_system[@]}"
+    fi
+    
+
+
+    echo "Removing Residual Dependencies" 
     if apt autoremove -y>/dev/null 2>&1; then
-        print_message "Kalıntılar silindi" \
-            "Residual dependencies removed"
+        echo "Residual dependencies removed"
     fi
 fi
